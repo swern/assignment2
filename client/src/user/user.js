b205b3e7369f4fa71033378a0cc49d0a8f0e9b87
davidedelerma <davide.delerma@gmail.com>
2016-07-29 10:23:10 +0100

    initial commit

---
+++ client/src/user/user.js
@@ -0,0 +1,4 @@
+//user constructor: name, surmname, bucketlist=[{location: ,price: ,event:, date:}]
+//prototipes:
+//can add event to bucket list
+//can remove event to bucket list 
---
+++ client/src/view/view.js
@@ -0,0 +1,11 @@
+//update bucket list
+//Method: find all the festivals in a certain timeframe and with a cost below the budget:
+//query the festival api
+// Method: for each festival find it's lat/lng from api
+// Method: use google api to find nearest airport
+//see if there are direct flights from user location 
+//Method: find flights from user location for wich price is < (fesitval ticket price + flight price)
+//Method: add a marker on the map for that flights with info window
+//Method: when click on marker add element to db (ask to Rick or Keith) and add element to info window (save button stuff)
+//Method: when click on element on info window remove marker element from info window and from database
+
