b240041707cb6afa4564d56d29da0256f241cf4f
davidedelerma <davide.delerma@gmail.com>
2016-07-31 18:45:36 +0100

    for loop

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -136,15 +136,19 @@
 	Airport.prototype = {
 	
 	  getAirports: function() {
-	    var url = "https://iatacodes.org/api/v6/nearby?api_key=0150cf1d-e183-4384-ad90-d4685a0c3454&lat="+this.airLat+"&lng="+this.airLng+" &distance=100";
-	    // var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=55.9486,-3.1999&radius=50000&type=airport&name=airport&key=AIzaSyB13OL9FrPlWcd8p3rZ_ASQy0nNK77R-ow"
+	    //console.log(this.airLat,this.airLng)
+	    var url = "https://airport.api.aero/airport/nearest/55.9486/-3.1999?maxAirports=10&user_key=3035d833bb6e531654a3cce03e6b1fde";
+	    // var url = "https://iatacodes.org/api/v6/nearby?api_key=0150cf1d-e183-4384-ad90-d4685a0c3454&lat="+this.airLat+"&lng="+this.airLng+" &distance=100&type=airport";
+	    // var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+this.airLat+","+this.airLng+"&radius=50000&type=airport&name=airport&key=AIzaSyB13OL9FrPlWcd8p3rZ_ASQy0nNK77R-ow"
 	    var request = new XMLHttpRequest();
 	    request.open("GET",url);
 	    request.setRequestHeader("Content-Type", "application/json")
 	    request.onload = function(){
 	      if (request.status === 200){
 	        var jsonString = request.responseText;
-	        this.airports = JSON.parse(jsonString)
+	        console.log(jsonString)
+	        
+	        //this.airports = JSON.parse(jsonString)
 	        this.onUpdate(this.airports)
 	      }
 	    }.bind(this)
--- server.js
+++ server.js
@@ -14,6 +14,33 @@ app.get("/", function(req,res){
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
     })
 
+// app.post("/bucketlist", function(req,res){
+//   MongoClient.connect(url, function(err, db){
+//     var collection = db.collection('state');
+//     collection.insert(req.body);
+//     res.status(200).end();
+//     db.close();
+//   })
+// })   
+
+// app.get("/bucketlist", function(req,res){
+//   MongoClient.connect(url, function(err, db){
+//     var collection = db.collection('state');
+//     collection.find({}).toArray(function(err, docs){
+//       res.json(docs);
+//       db.close();
+//     })
+//   })
+// })
+
+//   app.delete("/bucketlist/:id", function(req,res){
+//     MongoClient.connect(url, function(err, db){
+//       var collection = db.collection('state');
+//       collection.remove({_id: new ObjectID(req.params.id)})
+//       res.status(200).end();
+//       db.close();
+//     })
+//   })
 
 app.listen('3000', function(){
   console.log("run run running on 3000");
